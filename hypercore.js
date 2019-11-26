26d5c7c75def4d868c3d60c20650f051d721ff60
LorenzoEvans <lorenzo.evans94@gmail.com>
2019-11-25 19:37:24 -0500

    i like hypercore

--- hypercore.js
+++ hypercore.js
@@ -1,1 +1,17 @@
 const hypercore = require('hypercore')
+
+// Hypercore is an append only data structure
+
+const feed = hypercore('./single-chat-feed', {
+    valueEncoding: 'json'
+});
+
+feed.append({
+    type: 'chat-message',
+    nickname: 'cat-lover',
+    text: 'hello world',
+    timestamp: new Date().toISOString(),
+}, function (err, seq) {
+    if (err) throw err;
+    console.log('Data was appended as entry #' + seq)
+})
